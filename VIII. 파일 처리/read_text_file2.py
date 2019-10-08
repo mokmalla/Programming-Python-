2b5d06931926806229f5a592210011d719a5b43d
mokmalla <shl1933@naver.com>
2019-10-08 11:07:44 +0900

    write text(ama) file, read text(ama) file. readline, 가격의 총합

--- VIII. 파일 처리/read_text_file2.py
+++ VIII. 파일 처리/read_text_file2.py
@@ -3,7 +3,11 @@ while True:
     data =fi.readline()
     if not data:
         break
-    print(data, end="")
+    #print(data, end="")
+    l = data.split() #화이트스페이스 : \t,\n 띄어쓰기 있는 걸 다 자름
+    sum += int(l[1])
+    #총금액 계산하고 출력
+print("총 금액 : "+str(sum)+"원")
 fi.close()
 
 #한줄 안에서 이름 가격,
--- VIII. 파일 처리/write_text_file4.py
+++ VIII. 파일 처리/write_text_file4.py
@@ -1,7 +1,7 @@
 fi = open("history.ama", "w", encoding="utf8")
 
-fi.write("아메리카노\t2300원\t레귤러\t50%\t100%\t코코\n")
-fi.write("타로버블티\t3200원\t점보\t100%\t100%\t타피오카\n")
-fi.write("티라미수버블티\t3800원\t점보\t0%\t100%\t타피오카\n")
+fi.write("아메리카노\t2300원\t0\t1\t2\t1\n")
+fi.write("타로버블티\t3200원\t1\t2\t2\t0\n")
+fi.write("티라미수버블티\t3800원\t1\t0\t2\t0\n")
 
 fi.close()
