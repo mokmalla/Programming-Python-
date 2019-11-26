26bfc8c476d18c6c96810b4bb2dc016eaa3b81a0
mokmalla <shl1933@naver.com>
2019-11-26 12:07:18 +0900

    web crawling - 다음웹툰 json

---
+++ IX. 프로젝트/web_crawling_daumwebtoon.py
@@ -0,0 +1,17 @@
+#다음 웹툰 > 어쩌다 발견한 7월
+from urllib.request import urlopen
+import json
+
+
+
+if __name__ == '__main__':
+    response = urlopen("http://webtoon.daum.net/data/pc/webtoon/view/findjuly")
+    response_byte = response.read()
+    response_json = json.loads(response_byte)
+    #print(response_json['data']['webtoon']['webtoonEpisodes'][11]['title'])
+    cartoon_titles = response_json['data']['webtoon']['webtoonEpisodes']
+    for item in cartoon_titles:
+        title = item['title']
+        thumbnail = item['thumbnailImage']['url']
+        print(title)
+        print(thumbnail)
